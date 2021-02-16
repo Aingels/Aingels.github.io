a6bb3acb927c6c92837100624c2e37ca0c7de737
Anthony Ingels <znoveur.xbox@hotmail.fr>
2021-02-17 00:35:46 +0100

    tp4 v 1.1

--- TP JS 4/secondWorker.js
+++ TP JS 4/secondWorker.js
@@ -4,7 +4,7 @@ const numCouleur =Math.random() * 3;
 var couleur = ["green","black","blue"];
 const delay = Math.random() * 10;
 
-var time = setTimeout(pointRouge(), delay * 1000);
+var time = setTimeout(pointRand(), delay * 1000);
 
 function pointRand() {
 
--- TP JS 4/worker1.js
+++ TP JS 4/worker1.js
@@ -2,14 +2,22 @@ const x = Math.random() * 1000;
 const y = Math.random() * 1000;
 
 const delay = Math.random() * 10;
-
+const l_worker = [];
 var time = setTimeout(pointRouge(), delay * 1000);
 
+
 function pointRouge() {
+
+    postMessage([x, y, "red"]);
 
     for (var i = 0; i < 10; ++i) {
         l_worker.push(new Worker("secondWorker.js"));
     }
-    postMessage([x, y, "red"]);
+    for (var i =0;i<10;i++){
+        l_worker[i].onmessage = function(e){
+           postMessage(e.data);
+        }
+    }
 }
+
 
